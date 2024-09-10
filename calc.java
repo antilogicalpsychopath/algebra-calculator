5c123d6ed34c38a410d87678080790eb558a11d0
kasper <eshdesh@null.net>
2024-09-10 09:06:26 -0500

    Update calc.java

--- calc.java
+++ calc.java
@@ -1,99 +1,147 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.Scanner;
 
-public class AlgebraCalculator {
-
+public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        String equation;
 
-        while (true) {
-            System.out.println("Enter equation or exit: ");
-            equation = scanner.nextLine(); // more flexible input
+        JFrame frame = new JFrame("algebra calculator");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(300, 200);
 
-            if (equation.equalsIgnoreCase("exit")) { // EXIT would work too if ur rlly mad at my program :(
-                break;
-            }
+        JTextField textField = new JTextField(20);
+        textField.setPreferredSize(new Dimension(1, 1));
+        textField.setMaximumSize(new Dimension(200, 25));
+        textField.setHorizontalAlignment(JTextField.CENTER);
+        JButton button = new JButton("solve");
+        JLabel resultLabel = new JLabel("");
 
-            if (equation.contains("=")) {
-                String[] sides = equation.split("=");
-                String leftSide = sides[0].replaceAll(" ", ""); // NO SPACES
-                String rightOG = sides[1].replaceAll(" ", "");
+        button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String inputText = textField.getText();
 
-                double right = 0;
-                boolean isRightX = false;
+                Scanner scanner = new Scanner(inputText);
+                String equation;
+                equation = scanner.next(); // more flexible input
 
-                if (rightOG.equals("x")) {
-                    isRightX = true;
-                } else {
-                    right = Double.parseDouble(rightOG); // convert string to double which is just BIGger float
+                if (equation.equalsIgnoreCase("exit")) { // EXIT would work too if ur rlly mad at my program :(
+                    return;
                 }
 
-                if (leftSide.matches(".*[*/].*")) {
-                    System.out.println("error NO DIVISION OR * MULTPLICATION"); // If multiplication or division is present in algebraic expressions (too hard for the puny little baby 16 yr old i am)
-                } else {
-                    double coefficientX = 0;
-                    double constant = 0;
+                if (equation.contains("=")) {
+                    String[] sides = equation.split("=");
+                    String leftSide = sides[0].replaceAll(" ", ""); // NO SPACES
+                    String rightOG = sides[1].replaceAll(" ", "");
 
-                    String[] parts = leftSide.split("(?=[+-])"); // splits into different parts like "3x+3" -> "3x","+3"
+                    double right = 0;
+                    boolean isRightX = false;
 
-                    for (String part : parts) {
-                        if (part.contains("x")) { // if dealing with coeff eg 3x
-                            part = part.replace("x", "");
-                            if (part.equals("+") || part.equals("")) { // this bc +x is positive x but just x is also positive x
-                                coefficientX += 1;
-                            } else if (part.equals("-")) {
-                                coefficientX -= 1;
-                            } else {
-                                coefficientX += Double.parseDouble(part); // coeffx = coeffx+part (handles coeffeciants like 7x or -13x)
+                    if (rightOG.equals("x")) {
+                        isRightX = true;
+                    } else {
+                        right = Double.parseDouble(rightOG); // convert string to double which is just BIGger float
+                    }
+
+                    if (leftSide.matches(".*[*/].*")) {
+                        resultLabel.setText("error NO DIVISION OR * MULTPLICATION"); // If multiplication or division is present in algebraic expressions (too hard for the puny little baby 16 yr old i am)
+                    } else {
+                        double coefficientX = 0;
+                        double constant = 0;
+
+                        String[] parts = leftSide.split("(?=[+-])"); // splits into different parts like "3x+3" -> "3x","+3"
+
+                        for (String part : parts) {
+                            if (part.contains("x")) { // if dealing with coeff eg 3x
+                                part = part.replace("x", "");
+                                if (part.equals("+") || part.equals("")) { // this bc +x is positive x but just x is also positive x
+                                    coefficientX += 1;
+                                } else if (part.equals("-")) {
+                                    coefficientX -= 1;
+                                } else {
+                                    coefficientX += Double.parseDouble(part); // coeffx = coeffx+part (handles coeffeciants like 7x or -13x)
+                                }
+                            } else { // if dealing with not coeff eg 3
+                                constant += Double.parseDouble(part);
                             }
-                        } else { // if dealing with not coeff eg 3
-                            constant += Double.parseDouble(part);
+                        }
+
+                        if (coefficientX == 0) {
+                            if (isRightX) {
+                                resultLabel.setText("x = " + String.valueOf(constant));
+                            } else {
+                                resultLabel.setText("error!!!!"); // grrr WHY NO X?????????
+                            }
+                        } else {
+                            double x;
+                            if (isRightX) { // so if 3+3 or similar was input
+                                x = constant / coefficientX; // 6/1
+                            } else {
+                                right = Double.parseDouble(rightOG);
+                                x = (right - constant) / coefficientX; // the eq for basic algebra
+                            }
+                            resultLabel.setText("x = " + String.valueOf(x));
+                        }
+                    }
+                } else if (equation.matches("^[0-9]+([*/+-][0-9]+)*$")) {
+                    String[] addSubParts = equation.split("(?=[+-])"); // split addition and subtraction first
+                    double result = 0;
+
+                    for (String addSubPart : addSubParts) {
+                        double tempResult = 1;
+                        boolean isMultiplying = true;
+
+                        String[] mulDivParts = addSubPart.split("(?=[*/])"); // split multiplication and division
+                        for (String mulDivPart : mulDivParts) {
+                            if (mulDivPart.contains("*")) {
+                                isMultiplying = true;
+                                mulDivPart = mulDivPart.replace("*", "");
+                            } else if (mulDivPart.contains("/")) {
+                                isMultiplying = false;
+                                mulDivPart = mulDivPart.replace("/", "");
+                            }
+                            double value = Double.parseDouble(mulDivPart.trim());
+
+                            if (isMultiplying) {
+                                tempResult *= value;
+                            } else {
+                                tempResult /= value;
+                            }
+                        }
+
+                        if (addSubPart.startsWith("-")) {
+                            result -= tempResult;
+                        } else {
+                            result += tempResult;
                         }
                     }
 
-                    if (coefficientX == 0) {
-                        if (isRightX) {
-                            System.out.println("x: " + constant);
-                        } else {
-                            System.out.println("error!!!"); // grrr WHY NO X?????????
-                        }
-                    } else {
-                        double x;
-                        if (isRightX) { // so if 3+3 or similar was input
-                            x = constant / coefficientX; // 6/1
-                        } else {
-                            right = Double.parseDouble(rightOG);
-                            x = (right - constant) / coefficientX; // the eq for basic algebra
-                        }
-                        System.out.println("x: " + x);
-                    }
+                    resultLabel.setText("x = " + result);
+                } else {
+                    resultLabel.setText("error!!!"); // ew get out
                 }
-            } else if (equation.matches("^[0-9]+([*/][0-9]+)*$")) {
-                // regex is confusing to look at but pretty simple: just checking that it looks like this: 87*94 or 7/1
-                String[] parts = equation.split("(?=[*/])"); // split * or / for mult or div
-                double result = 1;
-                boolean isMultiplying = true;
 
-                for (String part : parts) { // literally just normal  */ calculator; making an actually correct algebra calculator with division and multiplication is hard
-                    if (part.contains("*")) {
-                        isMultiplying = true;
-                        part = part.replace("*", "");
-                    } else if (part.contains("/")) {
-                        isMultiplying = false;
-                        part = part.replace("/", "");
-                    }
-                    double value = Double.parseDouble(part);
-                    if (isMultiplying) {
-                        result *= value;
-                    } else {
-                        result /= value;
-                    }
-                }
-                System.out.println("x: " + result);
-            } else {
-                System.out.println("error!!"); // ew get out
+                scanner.close();
             }
-        }
-        scanner.close();
+        });
+
+        JPanel verticalPanel = new JPanel();
+        verticalPanel.setLayout(new BoxLayout(verticalPanel, BoxLayout.Y_AXIS));
+        verticalPanel.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+        verticalPanel.add(Box.createRigidArea(new Dimension(0, 20)));
+        verticalPanel.add(textField);
+        verticalPanel.add(Box.createRigidArea(new Dimension(0, 10)));
+        verticalPanel.add(button);
+        verticalPanel.add(Box.createRigidArea(new Dimension(0, 10)));
+        verticalPanel.add(resultLabel);
+
+        button.setAlignmentX(Component.CENTER_ALIGNMENT);
+        resultLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+        frame.add(verticalPanel);
+        frame.setVisible(true);
     }
 }
